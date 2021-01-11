446ea93440d9b80f3ee8e9c8b69170ccadf440bc
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-01-11 14:16:49 -0500

    added table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,10 @@
 // from data.js
 var tableData = data;
-
+var tbody = d3.select("tbody");
 // YOUR CODE HERE!
+tableData.forEach((ufoReport)=>{
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key,value])=>{
+        row.append("td").text(value);
+    });
+});
