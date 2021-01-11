d64e2fc4c3c1aedf2783a8c5e311dfdfde8d0705
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-01-11 15:43:36 -0500

    filtered by date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,10 +1,36 @@
 // from data.js
 var tableData = data;
+//reference tbody 
 var tbody = d3.select("tbody");
 // YOUR CODE HERE!
-tableData.forEach((ufoReport)=>{
+//function for buildtable 
+function buildtable(table){
+table.forEach((ufoReport)=>{
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key,value])=>{
         row.append("td").text(value);
     });
-});
+    });
+};
+
+var button = d3.select("#filter-btn");
+var form = d3.select(".form-group");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    let dateValue = inputElement.property("value");
+    let filterdata = tableData
+    // if a date exist.....
+    if (dateValue) {
+        filterdata = filterdata.filter(table=> table.datetime === dateValue)
+    }
+    tbody.html("")
+    buildtable(filterdata)
+
+    console.log(filterdata)
+}
+buildtable(tableData)
